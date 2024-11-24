b913683f9b18533bb421f1b99ca5c411f35c0dd1
Alberto Garcia <agarcia05@berkeley.edu>
2024-11-24 01:44:10 -0800

    modified

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,3 +1,5 @@
+import TextField from "@mui/material/TextField";
+
 export default function Navbar() {
     const logoref = "ASUC-logo.png";
     let menuitems = [
@@ -17,49 +19,42 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="flex flex-row">
-    <a className="basis-1/4 bg-green-500 flex justify-center items-center text-4xl">
-        <img className="w-50 h-50 mr-2" src={logoref} alt="Logo" />
-        ASUC OEVP
-    </a>
-    <ul className="basis-1/2 bg-blue-500 flex flex-row justify-center self-center">
-        <li className="p-8 text-2xl text-gray-800 flex items-center">
-            <a className="flex items-center">
-                <img className="w-6 h-6 mx-2" src={logos["1"]} alt="Icon 1" />
-                {menuitems[0]}
+        <nav className="flex flex-row bg-[#003A70] text-white">
+            <a className="basis-1/4  flex justify-center items-center text-4xl">
+                <img style={{ width: '61.261px', height: '57px' }} className="mr-2" src={logoref} alt="Logo" />
+                ASUC OEVP
             </a>
-        </li>
-        <li className="p-8 text-2xl text-gray-800 flex items-center">
-            <a className="flex items-center">
-                <img className="w-6 h-6 mx-2" src={logos["2"]} alt="Icon 2" />
-                {menuitems[1]}
-            </a>
-        </li>
-        <li className="p-8 text-2xl text-gray-800 flex items-center">
-            <a className="flex items-center">
-                <img className="w-6 h-6 mx-2" src={logos["3"]} alt="Icon 3" />
-                {menuitems[2]}
-            </a>
-        </li>
-        <li className="p-8 text-2xl text-gray-800 flex items-center">
-            <a className="flex items-center">
-                <img className="w-6 h-6 mx-2" src={logos["4"]} alt="Icon 4" />
-                {menuitems[3]}
-            </a>
-        </li>
-        <li className="p-8 text-2xl text-gray-800 flex items-center">
-            <a className="flex items-center">
-                <img className="w-6 h-6 mx-2" src={logos["5"]} alt="Icon 5" />
-                {menuitems[4]}
-            </a>
-        </li>
-    </ul>
-    <a className="basis-1/4 bg-green-500 flex justify-center items-center text-2xl text-gray-800 font-bold">
-        
-    </a>
-</nav>
+            <ul className="basis-1/2  flex flex-row justify-center text-white self-center">
+                <li className="p-8 text-2xl text-gray-800 flex items-center text-white">
+                    <a className="flex items-center">
+                        {menuitems[0]}
+                    </a>
+                </li>
+                <li className="p-8 text-2xl text-gray-800 flex items-center text-white">
+                    <a className="flex items-center">
+                        {menuitems[1]}
+                    </a>
+                </li>
+                <li className="p-8 text-2xl text-gray-800 flex items-center text-white">
+                    <a className="flex items-center">
+                        {menuitems[2]}
+                    </a>
+                </li>
+                <li className="p-8 text-2xl text-gray-800 flex items-center text-white">
+                    <a className="flex items-center">
+                        {menuitems[3]}
+                    </a>
+                </li>
+                <li className="p-8 text-2xl text-gray-800 flex items-center">
+                    <a className="flex items-center text-white">
+                        {menuitems[4]}
+                    </a>
+                </li>
+            </ul>
+            <a href="#" className="basis-1/4 bg-green flex justify-center items-center text-2xl text-gray-800 font-bold"></a>
 
-    );
+            
+        </nav> );
     
     
 }
