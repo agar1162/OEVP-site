7ac0bb38cd1b9c4a43810194b78b76ca58d14a88
Alberto Garcia <agarcia05@berkeley.edu>
2024-12-02 20:41:10 -0800

    Subscription and Stats

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,15 @@ import Navbar from "@/components/ui/Navbar";
 import "./globals.css";
 import Footer from "@/components/ui/Footer";
 import Cover from "@/components/ui/Cover";
+import Subscription from "@/components/ui/Subscription"
+import Stats from "@/components/ui/Stats"
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Cover />
+      <Stats />
+      <Subscription />
       <Footer />
     </div>
   );
--- src/components/ui/Cover.js
+++ src/components/ui/Cover.js
@@ -1,7 +1,9 @@
 
 export default function Cover(){
     return(
-        <div className="h-screen">
+        <div className="h-screen text-center" >
+            <h1 className="pt-10 text-[50px] text-[#F9B315]">Office of the Executive Vice President</h1>
+            
             
         </div>
     );
